2f5d11993aaac78ab81616c6c03cebddb4f2c079
kobato-chan1912 <no_body156@yahoo.com.vn>
2024-03-28 16:39:30 +0700

    fix bug

--- app/Console/Commands/AutoBidProcess.php
+++ app/Console/Commands/AutoBidProcess.php
@@ -49,12 +49,15 @@ class AutoBidProcess extends Command
 
 
         if ($isHighest == 0) {
+          // re-query
+          $auction = Auction::find($auction->id);
           $currentPrice = $auction->current_price;
           $bidPrice = $currentPrice + $auction->jump_price;
           if ($setting->max_price >= $bidPrice) {
             $checkRegister = AuctionRegister::where("auction_id", $auction->id)->where("user_id", $setting->user_id)->where("paid_status", "paid")->where("is_disable", 0);
             if ($checkRegister->exists() && ($bidPrice < $auction->buy_price)) {
               Bid::create(["auction_id" => $auction->id, "user_id" => $setting->user_id,
+                "status" => null, "tax_status" => null, "remain_status" => null,
                 "bid_price" => $bidPrice, "tax_price" => countTaxPrice($bidPrice),
                 "remain_price" => $bidPrice - ($auction->start_price * 10 / 100),
                 "auction_register_id" => $checkRegister->first()->id,
